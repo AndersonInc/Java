b3255a967c2e11454705f314f815e547d9151bb4
AndersonInc <97854779+andersoninc@users.noreply.github.com>
2023-03-13 10:11:32 +0300

    Variables

---
+++ JavaCourse/src/Variables.java
@@ -0,0 +1,24 @@
+//In this lesson we learn about variables
+/*
+* Variables are categorized into two i.e. primitive and non primitive
+* 1.Primitive includes the string,arrays,classes, interfaces
+* 2.Non primitive includes the numerical and non numerical
+*       a.Numerical includesL: short,long,int,float,double etc
+*       b.Non numerical includes char
+* Further more Variables can be classified as being:
+*   1.Static. These have a static keyword on them
+*   2.Local.These are defined with in a method body
+*   3.Instance. These don.t have the static key word in them
+*
+* A varible follows two things, you have to declare it and the initialize it.
+* Youcan do all the two at once
+* Lets see an example of the above*/
+public class Variables {
+    static int age = 23; //static variable
+    int amount = 5000; //Instance variable
+
+    void trade(){
+        double price = 2.5; //Local Variable
+    }
+
+}
